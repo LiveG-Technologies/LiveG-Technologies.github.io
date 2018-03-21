16c150e9009cd0555514d0f7105ae38332338571
LiveG <hi@liveg.tech>
2018-03-21 12:59:00 +0000

    Add functioning login

--- auroraservice/borealis.js
+++ auroraservice/borealis.js
@@ -1,47 +1,55 @@
 document.getElementById("notsupported").style.display = "none";
 
-function iframed() {
-    try {
-        return window.self !== window.top;
-    } catch (e) {
-        return true;
-    }
-}
-
-function checkFields(login = true) {
-    if (document.getElementById("user").value != "" && document.getElementById("pass").value != "") {
-        return true;
-    } else {
-        document.getElementById("error").innerHTML = "You have not filled out all of the required fields.";
-        return false;
-    }
-}
-
-function login() {
-    if (checkFields(true)) {
-        localStorage.setItem("username", document.getElementById("user").value);
-    }
-}
-
-function signup() {
-    checkFields(false);
-}
-
-function change() {
+function change(user) {
     if (typeof(Storage) !== "undefined") {
-        var user = localStorage.getItem("username");
-        var pass = localStorage.getItem("password");
-
-        if (user == null) {
-            document.getElementById("login").style.display = "unset";
-            document.getElementById("logout").style.display = "none";
-        } else {
+        if (user && user.uid != currentUid) {
+            // Sign in operation.
             document.getElementById("login").style.display = "none";
             document.getElementById("logout").style.display = "unset";
+        } else {
+            // Sign out operation.
+            document.getElementById("login").style.display = "unset";
+            document.getElementById("logout").style.display = "none";
         }
+
     } else {
         alert("Sorry! You will not be able to use your Borealis account on this device as it does not support HTML5 web storage.");
     }
 }
 
-change();
+var currentUid = null;
+firebase.auth().onAuthStateChanged(function(user) {
+    // Checks if user auth state has changed.
+    change(user);
+});
+
+function checkFields() {
+    if (document.getElementById("user").value != "" && document.getElementById("pass").value != "") {
+        return true;
+    } else {
+        document.getElementById("error").innerHTML = "Oops! You have not filled out all of the required fields.";
+        return false;
+    }
+}
+
+function login() {
+    if (checkFields()) {
+        firebase.auth().signInWithEmailAndPassword(document.getElementById("user").value, document.getElementById("pass").value).catch(function(error) {
+            document.getElementById("error").innerHTML = "Oops! " + error.message.replace(/email/g, "e-mail").replace(/E-mail/g, "E-mail");
+        });
+    }
+}
+
+function signup() {
+    if(checkFields()) {
+        firebase.auth().createUserWithEmailAndPassword(document.getElementById("user").value, document.getElementById("pass").value).catch(function(error) {
+            document.getElementById("error").innerHTML = "Oops! " + error.message.replace(/email/g, "e-mail").replace(/E-mail/g, "E-mail");
+        });
+    }
+}
+
+function logout() {
+    firebase.auth().signOut().catch(function(error) {
+        document.getElementById("error").innerHTML = "Oops! Something went wrong on our side. Try again soon!";
+    });
+}
