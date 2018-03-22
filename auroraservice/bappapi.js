d2592dae8e345f9ab8a38f1c34b8d21c6a3ea319
LiveG <hi@liveg.tech>
2018-03-22 17:58:13 +0000

    Add return to data

--- auroraservice/bappapi.js
+++ auroraservice/bappapi.js
@@ -53,6 +53,7 @@ var app = {
             firebase.database().ref("users/" + setup.userUid + "/" + setup.storedAs + "/" + tag).once("value").then(function(snapshot) {
                 data = snapshot.val();
             });
+            return data;
         } else if (setup.storedAs == null) {
             throw("App not properly initialised. Please fix this by using the `app.load` command.")
         }
