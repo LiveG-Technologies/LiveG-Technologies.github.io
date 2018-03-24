4602fc4bf978db2f658edfdf8300c46a115b0a59
LiveG <hi@liveg.tech>
2018-03-24 10:19:54 +0000

    Add username field to signup

--- auroraservice/borealis.js
+++ auroraservice/borealis.js
@@ -5,10 +5,12 @@ function change(user) {
         if (user && user.uid != currentUid) {
             // Sign in operation.
             document.getElementById("login").style.display = "none";
+            document.getElementById("signup").style.display = "none";
             document.getElementById("logout").style.display = "unset";
         } else {
             // Sign out operation.
             document.getElementById("login").style.display = "unset";
+            document.getElementById("signup").style.display = "none";
             document.getElementById("logout").style.display = "none";
         }
     } else {
@@ -31,7 +33,17 @@ function checkFields() {
     }
 }
 
+function checkUsername() {
+    if (document.getElementById("name").value != "") {
+        return true;
+    } else {
+        document.getElementById("error").innerHTML = "Oops! You have not filled out all of the required fields.";
+        return false;
+    }
+}
+
 function login() {
+    document.getElementById("error").innerHTML = "";
     if (checkFields()) {
         firebase.auth().signInWithEmailAndPassword(document.getElementById("user").value, document.getElementById("pass").value).catch(function(error) {
             document.getElementById("error").innerHTML = "Oops! " + error.message.replace(/email/g, "e-mail").replace(/E-mail/g, "E-mail");
@@ -39,15 +51,28 @@ function login() {
     }
 }
 
-function signup() {
+function signupBefore() {
+    document.getElementById("error").innerHTML = "";
     if(checkFields()) {
-        firebase.auth().createUserWithEmailAndPassword(document.getElementById("user").value, document.getElementById("pass").value).catch(function(error) {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("signup").style.display = "unset";
+        document.getElementById("logout").style.display = "none";
+    }
+}
+
+function signup() {
+    document.getElementById("error").innerHTML = "";
+    if(checkFields()) {
+        firebase.auth().createUserWithEmailAndPassword(document.getElementById("user").value, document.getElementById("pass").value).then(function() {
+            firebase.database().ref("users/" + firebase.auth().currentUser.uid + "/_settings/name").set(document.getElementById("name").value);
+        }).catch(function(error) {
             document.getElementById("error").innerHTML = "Oops! " + error.message.replace(/email/g, "e-mail").replace(/E-mail/g, "E-mail");
         });
     }
 }
 
 function logout() {
+    document.getElementById("error").innerHTML = "";
     firebase.auth().signOut().catch(function(error) {
         document.getElementById("error").innerHTML = "Oops! Something went wrong on our side. Try again soon!";
     });
