47f51ba98609f021329b62e6bac25ee45952aedd
LiveG <hi@liveg.tech>
2018-03-27 13:24:36 +0100

    Fix ID lengths

--- auroraservice/source/webcount.js
+++ auroraservice/source/webcount.js
@@ -1,15 +1,15 @@
 var webcount = {
     create: function(apiKey) {
-        if (apiKey.length == 48) {
-            user = apiKey.substring(0, 16);
-            site = apiKey.substring(16, 32);
-            page = apiKey.substring(32, 48);
+        if (apiKey.length == 68) {
+            user = apiKey.substring(0, 28);
+            site = apiKey.substring(28, 48);
+            page = apiKey.substring(48, 68);
 
             document.body += "<iframe src='https://liveg-technologies.github.io/auroraservice/embed/source/webcount.html?user=" + user + "&site=" + site + "&page=" + page + "' width='0' height='0' style='border: none;'>";
 
             console.log("Aurora Webocunt initialised successfully.");
         } else {
-            throw("The API key is invalid. It should be 48 characters long.");
+            throw("The API key is invalid. It should be 68 characters long.");
         }
     }
 }
