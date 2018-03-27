d46c21a5f5ffb28ee86409836af8a18ac445d69a
LiveG <hi@liveg.tech>
2018-03-27 12:53:12 +0100

    Create source folder

---
+++ auroraservice/source/webcount.js
@@ -0,0 +1,6 @@
+var webcount = {
+    create: function() {
+        document.body += "<iframe src='https://liveg-technologies.github.io/auroraservice/embed/source/webcount.html' width='0' height='0' style='border: none;'>";
+        console.log("Aurora Webocunt initialised successfully.");
+    }
+}
