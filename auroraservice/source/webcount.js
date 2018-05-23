ffe1cf34c43ce67158577e8d1dacbb36fb09771d
LiveG <hi@liveg.tech>
2018-05-23 13:55:56 +0100

    Add data getter to api

--- auroraservice/source/webcount.js
+++ auroraservice/source/webcount.js
@@ -1,4 +1,6 @@
 var webcount = {
+    apiKey: "",
+
     create: function(apiKey) {
         if (apiKey.length == 68) {
             user = apiKey.substring(0, 28);
@@ -7,9 +9,26 @@ var webcount = {
 
             document.body.innerHTML += "<iframe src='https://liveg-technologies.github.io/auroraservice/embed/source/webcount.html?user=" + user + "&site=" + site + "&page=" + page + "' width='0' height='0' style='border: none;'>";
 
+            webcount.apiKey = apiKey;
+
             console.log("Aurora Webcount initialised successfully.");
         } else {
             throw("The API key is invalid. It should be 68 characters long.");
         }
+    },
+
+    data: function(callback) {
+        var id = Math.floor(Math.random() * 100000);
+
+        div.insertAdjacentHTML("beforeend", "<iframe id='dataGet" + id + "' src='https://liveg-technologies.github.io/auroraservice/source/data.html?apiKey=" + webcount.apiKey + "' width='0' height='0' style='border: none;'></iframe>");
+
+        setInterval(function() {
+            try {
+                
+                if (document.getElementById("pendCheck").contentWindow.document.body.innerHTML.charAt(0) == "{") {
+                    callback(document.getElementById("pendCheck").contentWindow.document.getElementById("json").innerHTML);
+                }
+            } catch (e) {}
+        }, 100);
     }
 }
